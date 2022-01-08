54904283aca40f8355a5b929b4ce8dacac5cdca6
Gary Hu <gary.hu@msn.com>
2022-01-08 13:57:34 -0800

    u1

--- src/appointment/appointment.module.ts
+++ src/appointment/appointment.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { appointmentController } from './appointment.controller';
 import { appointmentService } from './appointment.service';
-import { StudyModule, StudyService } from '../study';
 
 @Module({
-  imports: [StudyModule], //need consider to avoid circular dependencies
+  // imports: [StudyModule], //need consider to avoid circular dependencies
   controllers: [appointmentController],
-  providers: [appointmentService, StudyService],
+  providers: [appointmentService],
 })
 export class appointmentModule {}
