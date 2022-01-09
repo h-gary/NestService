c8dfcb8e84394f351543df0c660938304e150fa7
Gary Hu <gary.hu@msn.com>
2022-01-08 18:58:21 -0800

    remove auth

--- src/study/study.controller.ts
+++ src/study/study.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Inject, Post } from '@nestjs/common';
+
 import { StudyService } from '.';
 
 @Controller('study')
+//@UseGuards(JwtAuth)
 export class StudyController {
   constructor(@Inject('studyService') private study: StudyService[]) {}
 
+  //@UseGuards(JwtAuth)
   @Post()
   start(): Promise<void> {
     //use service base on request.
